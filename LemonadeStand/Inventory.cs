99a51a1f27fb8864a947d96f6fe9127715aa554d
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2019-01-02 08:58:32 -0600

    Made the given classes/made some variables for some classes

---
+++ LemonadeStand/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Inventory
+    {
+        public int cups;
+        public int lemons;
+        public int iceCubes;
+        public int sugarCubes;
+
+        public Inventory()
+        {
+            cups = 0;
+            lemons = 0;
+            iceCubes = 0;
+            sugarCubes = 0;
+        }
+    }
+}
---
+++ LemonadeStand/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Store
+    {
+        public double cupCost;
+        public double lemonCost;
+        public double sugarCubeCost;
+        public double iceCubeCost;
+
+        public Store()
+        {
+            cupCost = 0.10;
+            lemonCost = 0.25;
+            sugarCubeCost = 0.25;
+            iceCubeCost = 0.25;
+        }
+    }
+}
